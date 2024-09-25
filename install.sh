16aeae035a1baef9a1f565417f118bf6c8b5d212
Edo Suseno <edosuseno@gmail.com>
2024-09-25 15:57:09 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -42,8 +42,8 @@ download_files()
 {
 	clear
   	echo "Downloading files from repo..."
-   	rm -f $DIR/mbit && wget -O $DIR/mbit https://raw.githubusercontent.com/aryobrokolly/ngebit/main/usr/bin/mbit && chmod +x $DIR/mbit
- 	rm -f $DIR/orbit.py && wget -O $DIR/orbit.py https://raw.githubusercontent.com/aryobrokolly/ngebit/main/usr/bin/orbit.py && chmod +x $DIR/orbit.py
+   	rm -f $DIR/mbit && wget -O $DIR/mbit https://raw.githubusercontent.com/edosuseno/ngebit/main/usr/bin/mbit && chmod +x $DIR/mbit
+ 	rm -f $DIR/orbit.py && wget -O $DIR/orbit.py https://raw.githubusercontent.com/edosuseno/ngebit/main/usr/bin/orbit.py && chmod +x $DIR/orbit.py
     	finish
 }
 
